f7fe9370257cf849cbdd6c11e44358319b2e7f69
Andrew Cobb <aecobb53@gmail.com>
2023-05-14 14:09:16 -0600

    adding stuff

--- main.py
+++ main.py
@@ -16,3 +16,25 @@ async def current_builds(requests: Request):
     with open('templates/main_page.html', 'r') as hf:
         html_content = hf.read()
     return HTMLResponse(content=html_content, status_code=200)
+
+@app.get('/test-get', resource_class=HTMLResponse)
+async def test_get_endpoint(requests: Request):
+    return HTMLResponse(status_code=200)
+
+@app.get('/test-get-data', resource_class=HTMLResponse)
+async def test_get_endpoint(requests: Request):
+    data = {
+        'results': [
+            {}
+        ]
+    }
+    return HTMLResponse(status_code=200)
+
+@app.post('/test-post', resource_class=HTMLResponse)
+async def test_post_endpoint(requests: Request):
+    return HTMLResponse(status_code=200)
+
+@app.put('/test-put', resource_class=HTMLResponse)
+async def test_put_endpoint(requests: Request):
+    return HTMLResponse(status_code=200)
+
